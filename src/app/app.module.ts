f0928b41b6abfe8426f498e670a3785c6430b85f
Macarena Carreño <mac.carreno@duocuc.cl>
2024-11-26 01:09:49 -0300

    Actualizacion Rama user.jpg borrado y app.module.ts corregido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule, ReactiveFormsModule],
