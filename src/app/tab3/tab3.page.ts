79168caef3e8f85380deba181784a1d55f2afa8a
Macarena Carreño <mac.carreno@duocuc.cl>
2024-11-25 22:05:45 -0300

    Revert "Creación de interfaces, creación de service, creación de get (para ver los eventos) , adición de foto "User""
    This reverts commit 6bb96fc13b05ce6e304fcc0f7f89664a028a14e4.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { ApicrudService } from '../services/apicrud.service';
-import { Eventos } from 'src/interfaces/eventos';
 
 
 @Component({
@@ -13,13 +11,9 @@ import { Eventos } from 'src/interfaces/eventos';
 })
 export class Tab3Page {
 
-  eventos: Eventos[]=[];
-
-
   constructor(private menucontroller: MenuController,
               private router: Router,
-              private alertcontroller: AlertController,
-              private apicrud:ApicrudService) {}
+              private alertcontroller: AlertController) {}
 
 mostrarMenu(){
   this.menucontroller.open('first');
@@ -27,11 +21,6 @@ mostrarMenu(){
 
 
 ngOnInit() {
-
-  this.apicrud.getEventos().subscribe( data => {
-    this.eventos = data; // Asegúrate de que 'data' contiene la propiedad 'imagen'
-  })
-
 }
 
 
