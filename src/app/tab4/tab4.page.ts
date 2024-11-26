4bcb5504791f7f9d93ca9edf888c510f2614aef2
JorgeLorca <j.lorca@duocuc.cl>
2024-11-26 13:50:22 -0300

    Implementacion de camara para escanear qr y build de la app

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { BrowserMultiFormatReader } from '@zxing/library';
 
 @Component({
   selector: 'app-tab4',
@@ -7,6 +9,8 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
+  scannedData: string[]=[];
+  aux: boolean=false;
 
   constructor(private menucontroller: MenuController) { }
 
@@ -18,6 +22,36 @@ export class Tab4Page implements OnInit {
 
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.aux=false;
+    console.log(this.aux)
+  }
+
+  async scanQr() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      source: CameraSource.Camera,
+      resultType: CameraResultType.Base64,
+    });
+
+    // Aquí puedes manejar la imagen obtenida
+    const base64Image = 'data:image/jpeg;base64,' + image.base64String;
+    console.log(base64Image);
+    // Puedes usar base64Image para mostrarlo o procesarlo
+
+    const codeReader = new BrowserMultiFormatReader();
+    codeReader.decodeFromImage(undefined, base64Image).then((result) => {
+        console.log(result);
+        this.scannedData = result.toString().split('\n');
+        this.aux=true;
+        // Maneja el resultado del escaneo aquí
+    }).catch((err) => {
+        console.error(err);
+        this.scannedData = ['Error al escanear'];
+        this.aux=true;
+    });
   }
 
 }
